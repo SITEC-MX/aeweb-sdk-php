ecd39a94a03addcf24c832879dfb53c603a36d49
José Juan Martínez Peláez <josejuan@mpsoft.dev>
2026-07-02 23:24:27 -0600

    Corrección de codificación

--- src/AEWeb.php
+++ src/AEWeb.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Sistemas Especializados e Innovación Tecnológica, SA de CV
+ * Sistemas Especializados e InnovaciÃ³n TecnolÃ³gica, SA de CV
  * SiTEC AE - Administrador de Empresas Web
  *
  * v.1.0.0.0 - 2021-02-19
@@ -67,7 +67,7 @@ class AEWeb
                 }
                 else // Si no se proporciona la variable requerida
                 {
-                    throw new Exception("No se proporcionó la variable '{$variable_nombre}'.");
+                    throw new Exception("No se proporcionÃ³ la variable '{$variable_nombre}'.");
                 }
             }
             else // Si no es una variable
@@ -106,7 +106,7 @@ class AEWeb
         }
         catch(Throwable $t) // Error al generar la URL de la llamada
         {
-            $estado = array("estado"=>AEWeb::ENDPOINT_ERROR_GENERAR, "mensaje"=>"Error al generar la URL de la llamada.", "debug"=> utf8_encode($t->getMessage()));
+            $estado = array("estado"=>AEWeb::ENDPOINT_ERROR_GENERAR, "mensaje"=>"Error al generar la URL de la llamada.", "debug"=> $t->getMessage());
         }
 
         if($endpoint_url) // Si se obtiene la URL de la llamada
